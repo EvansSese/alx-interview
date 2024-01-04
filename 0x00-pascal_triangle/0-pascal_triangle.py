4babc18e558a9954ac94c4c77b0850a5ded41f15
Evans Sese <sese_evans@yahoo.com>
2024-01-05 00:23:51 +0300

    [Feature] Task 0

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,18 +5,18 @@ Pascal triangle
 
 
 def pascal_triangle(n):
-  """
-  create the triangle
-  """
-  if n <= 0:
-      return []
+    """
+    create the triangle
+    """
+    if n <= 0:
+        return []
 
-  triangle = [[1]]
-  for i in range(1, n):
-      row = [1]
-      for j in range(1, i):
-          row.append(triangle[i-1][j-1] + triangle[i-1][j])
-      row.append(1)
-      triangle.append(row)
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
 
-  return triangle
+    return triangle
