4a964175df73a6f210773722e428696d54b0164d
Evans Sese <sese_evans@yahoo.com>
2024-01-19 10:15:11 +0300

    [Feature] Task 0

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,19 +4,18 @@
 
 def minOperations(n):
     """Finding minimum operations"""
-    if n > 1:
-        dp = [float('inf')] * (n + 1)
-        dp[1] = 0
+    if not isinstance(n, int):
+        return 0
 
-        for i in range(2, n + 1):
-            for j in range(1, int(i ** 0.5) + 1):
-                if i % j == 0:
-                    dp[i] = min(dp[i], dp[j] + i // j)
-                    dp[i] = min(dp[i], dp[i // j] + j)
-
-        return dp[n]
-
-    return 0
+    operations = 0
+    iterator = 2
+    while iterator <= n:
+        if not (n % iterator):
+            n = int(n / iterator)
+            operations += iterator
+            iterator = 1
+        iterator += 1
+    return operations
 
 
 if __name__ == "__main__":
