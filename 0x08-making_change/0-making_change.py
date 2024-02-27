013fdad40310a3f41288d6064983b7f09cfde8a6
Evans Sese <sese_evans@yahoo.com>
2024-02-27 19:05:34 +0300

    [Feature] Task 0

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -6,10 +6,13 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
+    coins.sort(reverse=True)
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
 
     for coin in coins:
+        if coin > total:
+            continue
         for i in range(coin, total + 1):
             dp[i] = min(dp[i], dp[i - coin] + 1)
 
